2340dc2520cf0120270c8930784fb095f95c7f1d
RJW20 <robertjwatts84@outlook.com>
2024-06-06 19:11:06 +0100

    Change imports to absolute rather than relative

--- car_drive_ai_neat/main.py
+++ car_drive_ai_neat/main.py
@@ -1,8 +1,8 @@
 import neat
 
-from .player import Player
-from .settings import settings
-from .simulator import simulate
+from car_drive_ai_neat.player import Player
+from car_drive_ai_neat.settings import settings
+from car_drive_ai_neat.simulator import simulate
 
 
 def main() -> None:
